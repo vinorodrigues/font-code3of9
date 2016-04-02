5897a922639d6b2511c07e8db2f63bfbc41bb271
Vino Rodrigues <github@m.vinorodrigues.com>
2016-04-02 19:23:59 +1100

    added web-font

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ECHO Compiling code3of9 ...
-PHP code3of9.php > build/code3of9.sfd
+PHP code3of9.php > font/code3of9.sfd
 
 ECHO Compiling code3of9-bold ...
-PHP code3of9-bold.php > build/code3of9-bold.sfd
+PHP code3of9-bold.php > font/code3of9-bold.sfd
 
 ECHO Done.
